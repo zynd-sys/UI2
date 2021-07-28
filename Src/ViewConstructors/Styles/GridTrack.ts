3f60235673c7f820ff852c397a6bb0cb70f8aab1
zynd-sys <you@example.com>
2021-07-28 14:58:55 +0300

    Redesigned GridTrack
    * added methods grow, min, max
    * reworked toString and constructor

--- Src/ViewConstructors/Styles/GridTrack.ts
+++ Src/ViewConstructors/Styles/GridTrack.ts
@@ -4,13 +4,25 @@ import { Units } from "../Enum/Units";
 
 
 export class GridTrackClass {
+
+	protected growValue?: number
+	protected maxValue?: string
 	protected value: string
+	protected minValue?: string
+
+	public toString(): string {
+		let result = this.minValue || this.maxValue ? `clamp(${this.minValue || 0},${this.value},${this.maxValue || '100%'})` : this.value
+		return this.growValue ? `minmax(${result},${this.growValue}fr)` : result
+	}
 
 
-	public toString(): string { return this.value }
+
+	public grow(value: number): this { this.growValue = value; return this }
+	public min(value: number, unit: Units = Units.px): this { this.minValue = value.toString() + unit; return this }
+	public max(value: number, unit: Units = Units.px): this { this.maxValue = value.toString() + unit; return this }
 
 
-	constructor(minValue: number, minUnit: Units = Units.px,maxValue: number = 1, maxUnit: Units | 'fr' = 'fr'  ) { this.value = `minmax(${minValue}${minUnit},${maxValue}${maxUnit}`; }
+	constructor(value: number, unit: Units = Units.px) { this.value = value.toString() + unit; }
 }
 
 export function GridTrack(value: number, unit: Units = Units.px): GridTrackClass { return new GridTrackClass(value, unit) }
